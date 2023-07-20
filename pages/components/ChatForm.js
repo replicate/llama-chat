142b1b41543170aee08231cdc30d9246e26015f2
Paul Hayes <fofr@users.noreply.github.com>
2023-07-20 11:46:28 +0100

    Position chat form at the bottom

--- pages/components/ChatForm.js
+++ pages/components/ChatForm.js
@@ -1,15 +1,21 @@
 const ChatForm = ({ onSubmit }) => {
   return (
-    <form className="w-full flex" onSubmit={onSubmit}>
-      <input
-        type="text"
-        className="flex-grow"
-        name="prompt"
-      />
-      <button className="button" type="submit">
-        Send
-      </button>
-    </form>
+    <div className="relative">
+      <div className="fixed bottom-0 left-0 right-0 bg-slate-100 border-t-2">
+        <div className="container max-w-2xl mx-auto p-5 pb-8">
+          <form className="w-full flex" onSubmit={onSubmit}>
+            <input
+              type="text"
+              className="flex-grow rounded-l-md"
+              name="prompt"
+            />
+            <button className="button" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
