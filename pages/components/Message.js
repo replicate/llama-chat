4fb25c8998ee8131ce2536c950a688e9308eed85
Charlie Holtz <charlieholtz@gmail.com>
2023-07-26 12:43:14 -0400

    rounded-md

--- pages/components/Message.js
+++ pages/components/Message.js
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
 
 const Message = ({ message, isUser }) => {
-  let containerClass = "mt-5 bg-green-200 p-5 rounded-t-xl rounded-bl-xl";
+  let containerClass = "mt-5 bg-green-200 p-5 rounded-md";
   if (isUser) {
-    containerClass = "mt-5 bg-gray-100 p-5 rounded-t-xl rounded-br-xl";
+    containerClass = "mt-5 bg-gray-100 p-5 rounded-md";
   }
 
   console.log({ message });
@@ -14,13 +14,15 @@ const Message = ({ message, isUser }) => {
 
   return (
     <div className="">
-      <div className={containerClass}>
+      <div>
         {message &&
           message.split("\n").map((text, index) => (
-            <Fragment key={index}>
-              {text}
-              <br />
-            </Fragment>
+            <div class="">
+              <div className={containerClass} key={index}>
+                {text}
+                <br />
+              </div>
+            </div>
           ))}
       </div>
     </div>
