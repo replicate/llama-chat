aeacc065d7207b5442c85e7084222dd0eb165dee
Charlie Holtz <charlieholtz@gmail.com>
2023-07-27 20:10:35 -0400

    fix failed build

--- pages/components/SlideOver.js
+++ pages/components/SlideOver.js
@@ -92,7 +92,7 @@ export default function SlideOver({
                                   <div className="relative mt-1">
                                     <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left border border-gray-300 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                                       <span className="block truncate">
-                                        {size.name}
+                                        {size ? size.name : "loading..."}
                                       </span>
                                       <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                         <ChevronUpDownIcon
@@ -108,41 +108,45 @@ export default function SlideOver({
                                       leaveTo="opacity-0"
                                     >
                                       <Listbox.Options className="absolute mt-1 max-h-60 w-full shadow-md overflow-auto border-gray-700 rounded-md bg-white py-1 text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                        {versions.map((version, versionIdx) => (
-                                          <Listbox.Option
-                                            key={versionIdx}
-                                            className={({ active }) =>
-                                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                active
-                                                  ? "bg-gray-100 text-gray-900"
-                                                  : "text-gray-900"
-                                              }`
-                                            }
-                                            value={version}
-                                          >
-                                            {({ selected }) => (
-                                              <>
-                                                <span
-                                                  className={`block truncate ${
-                                                    selected
-                                                      ? "font-medium"
-                                                      : "font-normal"
-                                                  }`}
+                                        {versions
+                                          ? versions.map(
+                                              (version, versionIdx) => (
+                                                <Listbox.Option
+                                                  key={versionIdx}
+                                                  className={({ active }) =>
+                                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                      active
+                                                        ? "bg-gray-100 text-gray-900"
+                                                        : "text-gray-900"
+                                                    }`
+                                                  }
+                                                  value={version}
                                                 >
-                                                  {version.name}
-                                                </span>
-                                                {selected ? (
-                                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
-                                                    <CheckIcon
-                                                      className="h-5 w-5"
-                                                      aria-hidden="true"
-                                                    />
-                                                  </span>
-                                                ) : null}
-                                              </>
-                                            )}
-                                          </Listbox.Option>
-                                        ))}
+                                                  {({ selected }) => (
+                                                    <>
+                                                      <span
+                                                        className={`block truncate ${
+                                                          selected
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                        }`}
+                                                      >
+                                                        {version.name}
+                                                      </span>
+                                                      {selected ? (
+                                                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
+                                                          <CheckIcon
+                                                            className="h-5 w-5"
+                                                            aria-hidden="true"
+                                                          />
+                                                        </span>
+                                                      ) : null}
+                                                    </>
+                                                  )}
+                                                </Listbox.Option>
+                                              )
+                                            )
+                                          : null}
                                       </Listbox.Options>
                                     </Transition>
                                   </div>
