29073d10fb6c76a7fda94cba2ba0046b0bc0ac02
Charlie Holtz <charlieholtz@gmail.com>
2023-07-31 12:17:03 -0400

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default App;
