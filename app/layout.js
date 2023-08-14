cf52613d132dbc911ee12f3ccd9a3a61deda79fa
Ben Firshman <ben@firshman.com>
2023-08-14 08:52:08 -0700

    Add Replicate banner

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
+        <title>Chat with Llama 2</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
