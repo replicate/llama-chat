dd3a99c3bdec2e80cb727d0717927751f50b6383
Zeke Sikelianos <zeke@sikelianos.com>
2024-04-15 12:05:58 -0700

    hide metrics until they exist

--- app/components/Metrics.js
+++ app/components/Metrics.js
@@ -19,9 +19,13 @@ export default function Metrics({
   const tokensPerSecond =
     tokenCount > 0 && runningDuration > 0 && tokenCount / runningDuration;
 
+  if (timeToFirstToken === null || tokenCount === null || runningDuration === null) {
+    return null;
+  }
+
   return (
-    <dl className="grid grid-cols-12 gap-2">
-      <div className="col-span-3 sm:col-span-4 flex items-center justify-center py-4 sm:text-sm text-xs">
+    <dl className="grid grid-cols-12 gap-2 mb-4">
+      <div className="col-span-3 sm:col-span-4 flex items-center justify-center sm:text-sm text-xs">
         <dd className="text-gray-900 pr-3">
           {timeToFirstToken ? timeToFirstToken.toFixed(2) : "—"}
         </dd>
@@ -29,7 +33,7 @@ export default function Metrics({
           <span className="hidden sm:inline">sec to </span> first token
         </dt>
       </div>
-      <div className="col-span-3 flex items-center justify-center py-4 sm:text-sm text-xs">
+      <div className="col-span-3 flex items-center justify-center sm:text-sm text-xs">
         <dd className="text-gray-900 pr-2">
           {tokensPerSecond ? tokensPerSecond.toFixed(2) : "—"}
         </dd>
@@ -38,11 +42,11 @@ export default function Metrics({
           <span className="hidden sm:inline">ec</span>
         </dt>
       </div>
-      <div className="col-span-3 sm:col-span-2 flex items-center justify-center py-4 sm:text-sm text-xs">
+      <div className="col-span-3 sm:col-span-2 flex items-center justify-center sm:text-sm text-xs">
         <dd className="text-gray-900 pr-2">{tokenCount || "—"}</dd>
         <dt className="font-medium text-gray-500">tokens</dt>
       </div>
-      <div className="col-span-3 flex items-center justify-center py-4 sm:text-sm text-xs">
+      <div className="col-span-3 flex items-center justify-center sm:text-sm text-xs">
         <dd className="text-gray-900 pr-2">
           {Math.max(runningDuration, 0).toFixed(2)}
         </dd>
