78812297b4558479b75bad5bc85c0cf801e9307c
cbh123 <charlieholtz@gmail.com>
2024-04-16 15:50:11 -0700

    add model.id

--- app/components/Dropdown.js
+++ app/components/Dropdown.js
@@ -27,7 +27,7 @@ export default function Dropdown({ models, selectedModel, setModel }) {
         <Menu.Items className="absolute left-0 z-10 mt-2 w-72 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {models.map((model) => (
-              <Menu.Item>
+              <Menu.Item key={model.id}>
                 <button
                   className={`text-left text-gray-700 block w-full px-4 py-2 text-sm hover:bg-gray-100`}
                   onClick={() => setModel(model)}
