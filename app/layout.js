aa405429ad81a5c266fb95bdaa96a5dc7a7dea9b
cbh123 <charlieholtz@gmail.com>
2024-04-17 16:01:47 -0700

    new name - llama3

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
-export const metdata = {
-  title: "Llama Chat - Powered by Replicate",
+export const metadata = {
+  title: "Chat with Llama 3 on Replicate",
   openGraph: {
-    title: "Llama Chat - Powered by Replicate",
-    description: "I can explain concepts, write poems and code, solve logic puzzles, or even name your pets.",
+    title: "Chat with Llama 3 on Replicate",
+    description: "Llama 3 is the latest language model from Meta AI.",
   },
 };
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Llama Chat - Powered by Replicate</title>
+        <title>Chat with Llama 3 on Replicate</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
