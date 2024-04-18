9f3aa551c68f2c3914d1a941dac79ba8c7f79d06
Ben Firshman <ben@firshman.com>
2024-04-18 11:01:46 -0700

    Chat with

--- app/page.js
+++ app/page.js
@@ -241,7 +241,9 @@ export default function HomePage() {
     <>
       <CallToAction />
       <nav className="sm:pt-8 pt-4 px-4 sm:px-12 flex items-center">
-        <div className="text-xl pr-3 font-semibold text-gray-500">Talk to</div>
+        <div className="text-xl pr-3 font-semibold text-gray-500">
+          Chat with
+        </div>
         <div className="font-semibold text-gray-500 sm:text-center">
           <Dropdown models={MODELS} selectedModel={model} setModel={setModel} />
         </div>
