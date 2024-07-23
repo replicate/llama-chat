2c6ae4a7cdeafdd9355d79887a4abe0266eb8949
deepfates <deepfates@gmail.com>
2024-07-23 09:23:46 -0700

    update title

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,9 @@ import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 export const metadata = {
-  title: "Chat with Meta Llama 3 on Replicate",
+  title: "Chat with Meta Llama 3.1 on Replicate",
   openGraph: {
-    title: "Chat with Meta Llama 3 on Replicate",
+    title: "Chat with Meta Llama 3.1 on Replicate",
     description: "Llama 3 is the latest language model from Meta.",
   },
 };
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Chat with Meta Llama 3 on Replicate</title>
+        <title>Chat with Meta Llama 3.1 on Replicate</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
